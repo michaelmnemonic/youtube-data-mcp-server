a5bcc8792f1f60f4065e7e3b3869585881cc04b9
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-05 22:23:05 +0200

    Add initial flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "MCP Server for YouTube";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.aarch64-linux.youtube-data-mcp-server = nixpkgs.legacyPackages.aarch64-linux.buildNpmPackage {
+      pname = "youtube-data-mcp-server";
+      version = "0.0.1";
+
+      src = ./.;
+
+      npmDepsHash = "sha256-RTROQkoOpRUiBvB1zXittXB1HgtBN+GLTD0tx+ZkWaA=";
+    };
+
+    packages.aarch64-linux.default = self.packages.aarch64-linux.youtube-data-mcp-server;
+
+    devShells.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.mkShell {
+      buildInputs = with nixpkgs.legacyPackages.aarch64-linux; [
+        nodejs
+        prefetch-npm-deps
+      ];
+    };
+
+  };
+}
